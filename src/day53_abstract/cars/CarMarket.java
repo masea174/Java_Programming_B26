24e67989115b4897667398dd6c91bf75afa67a73
persik <diana.bulgac@gmail.com>
2022-04-28 10:37:07 -0400

    changes to CarMarket.java

--- src/day53_abstract/cars/CarMarket.java
+++ src/day53_abstract/cars/CarMarket.java
@@ -8,5 +8,14 @@ public class CarMarket {
         System.out.println();
         Toyota car2=new Toyota();
         car2.start();
-    }
-}
+
+    //    Car car2=new Car() cant create an object its an abstratct class same thing with ElectricCar that is abstract
+
+
+        System.out.println();
+        Tesla car4=new Tesla();
+        car4.charge();
+        car4.start();
+        //tesla is the concrete class first class to inherit from the abstract clases
+
+}}
--- src/day53_abstract/cars/Tesla.java
+++ src/day53_abstract/cars/Tesla.java
@@ -12,3 +12,4 @@ public class Tesla extends ElectricCar{
         System.out.println("Plug the cord to charge it");
     }
 }
+//the abstract classes are the classes where you create the abstract methods , and then in the concrete classes the clases that will inherit the abstract clases you have to impliment the methods otherway it will not compile. i the regular classes you cannot have abstract methods.
